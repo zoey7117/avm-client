67f319075d29a29df984c099243c26d6aa565415
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-06-15 22:40:46 -0400

    deleted animal component

--- src/components/animals/Animals.js
+++ src/components/animals/Animals.js
@@ -14,13 +14,89 @@ class Animals extends React.Component {
 			<div>
 				<div>
 					<div className="animal-container">
-						<div className="animal-child yes">True</div>
+						<div className="animal-child yes">
+							True
+							<div className="animal-true">
+								<h1>
+									<ul>
+										<li className="alive">Is it alive?</li>
+										<li className="extinct">Is it extinct?</li>
+										<li className="mammal">Is it a mammal?</li>
+										<li className="amphibian">Is it an amphibian?</li>
+										<li className="arms">Does it have arms?</li>
+										<li className="legs">Does it have legs?</li>
+										<li className="fly">Does it fly?</li>
+										<li className="livesInWater">Does it live in water?</li>
+										<li className="fur">Does it have fur?</li>
+										<li className="famous">Is it famous?</li>
+										<li className="male">Is it male?</li>
+										<li className="actor">Is it an actor?</li>
+										<li className="athelete">Is it an athelete?</li>
+										<li className="musician">Is it a musician?</li>
+										<li className="politician">Is it a politician?</li>
+										<li className="singer">Is it a singer?</li>
+										<li className="awardWinner">Is it an award winner?</li>
+										<li className="ageAbove40">Is it more than 40 years old?</li>
+									</ul>
+								</h1>
+							</div>
+						</div>
 
 						<div className="animal-child questions">
-							<h1>Is it bigger than a breadbox? {randomAnimal.name}</h1>
+							<h1>
+								<div className="breadbox-question">
+									Is it bigger than a breadbox? {String(randomAnimal.breadbox)}
+								</div>
+								{/* <hr /> */}
+								<br />
+								<div className="inside-container">
+									<li className="alive">Is it alive?</li>
+									<li className="extinct">Is it extinct?</li>
+									<li className="mammal">Is it a mammal?</li>
+									<li className="amphibian">Is it an amphibian?</li>
+									<li className="arms">Does it have arms?</li>
+									<li className="legs">Does it have legs?</li>
+									<li className="fly">Does it fly?</li>
+									<li className="livesInWater">Does it live in water?</li>
+									<li className="fur">Does it have fur?</li>
+									<li className="famous">Is it famous?</li>
+									<li className="male">Is it male?</li>
+									<li className="actor">Is it an actor?</li>
+									<li className="athelete">Is it an athelete?</li>
+									<li className="musician">Is it a musician?</li>
+									<li className="politician">Is it a politician?</li>
+									<li className="singer">Is it a singer?</li>
+									<li className="awardWinner">Is it an award winner?</li>
+									<li className="ageAbove40">Is it more than 40 years old?</li>
+								</div>
+							</h1>
 							{console.log(randomAnimal)}
 						</div>
-						<div className="animal-child no">Not True</div>
+						<div className="animal-child no">
+							Not True
+							<div className="animal-false">
+								<h1>
+									<li className="alive">Is it alive?</li>
+									<li className="extinct">Is it extinct?</li>
+									<li className="mammal">Is it a mammal?</li>
+									<li className="amphibian">Is it an amphibian?</li>
+									<li className="arms">Does it have arms?</li>
+									<li className="legs">Does it have legs?</li>
+									<li className="fly">Does it fly?</li>
+									<li className="livesInWater">Does it live in water?</li>
+									<li className="fur">Does it have fur?</li>
+									<li className="famous">Is it famous?</li>
+									<li className="male">Is it male?</li>
+									<li className="actor">Is it an actor?</li>
+									<li className="athelete">Is it an athelete?</li>
+									<li className="musician">Is it a musician?</li>
+									<li className="politician">Is it a politician?</li>
+									<li className="singer">Is it a singer?</li>
+									<li className="awardWinner">Is it an award winner?</li>
+									<li className="ageAbove40">Is it more than 40 years old?</li>
+								</h1>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
