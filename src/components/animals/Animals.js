99371399c8ecd9892bd372e27ad11cb6e1588806
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-06-18 17:39:58 -0400

    yesno columns show up

--- src/components/animals/Animals.js
+++ src/components/animals/Animals.js
@@ -1,8 +1,7 @@
 import React from 'react';
-// import Animal from '../stash/Animal';
 import './Animal.css';
-let yes = document.querySelectorAll('.flex-animal.yes');
-let no = document.querySelectorAll('.flex-animal.no');
+let yes = document.querySelectorAll('.yes');
+let no = document.querySelectorAll('.no');
 
 class Animals extends React.Component {
 
@@ -14,10 +13,30 @@ class Animals extends React.Component {
 		// }
 	}
 
+addListYes() {
+	document.querySelector(".yes").classList.add('show-yes')
+}
+
+addListNo() {
+	document.querySelector(".no").classList.add('show-no')
+}
+// addList() {
+// 	document.querySelector(".yes").classList.add('show-yes')
+
+
+
+// 	}
+
 	render() {
 		let animals = this.props.animals.animals;
 		let randomAnimal = animals[Math.floor(Math.random() * animals.length)];
 		console.log('random animal', randomAnimal);
+		let yes = document.querySelectorAll('.flex-animal.yes');
+		let yeses = document.querySelector('.yes');
+
+		let animalEx = document.getElementsByClassName('flex-animal yes')
+		console.log('animalEx', animalEx, 'yes', yes, 'yeses', yeses)
+		console.log(yes)
 
 		if (!randomAnimal) return null;
 
@@ -65,18 +84,19 @@ class Animals extends React.Component {
 									<h3>{String(randomAnimal.breadbox)}</h3>
 									<li className="alive" >Is it alive?</li>
 									<li className="extinct" onClick={() => console.log("extinct clicked!")}>Is it extinct?</li>
-									<li className="mammal" >Is it a mammal?</li>
+									<li className="mammal" a href="#" onClick={randomAnimal.mammal === true ? this.addListYes : null} >Is it a mammal?</li>
 									<li className="amphibian" onClick={() => console.log("amphibian clicked!", this.props)}>Is it an amphibian?</li>
 									<li className="arms" onClick={() => console.log("arms clicked!", randomAnimal.arms)} >Does it have arms?</li>
 									<li className="legs" onClick={() => this.isClicked(randomAnimal.legs)}>Does it have legs?</li>
-									{/* <li className="fly" onClick={randomAnimal.fly ? yes.classList.add('show-yes') : no.classList.add('show-no')}>Does it fly?</li> */}
-									<li className="livesInWater">Does it live in water?</li>
-									<li className="fur">Does it have fur?</li>
-									<li className="famous">Is it famous?</li>
-									<li className="male">Is it male?</li>
-									<li className="actor">Is it an actor?</li>
-									<li className="athelete">Is it an athelete?</li>
-									<li className="musician">Is it a musician?</li>
+									<li className="fly" >Does it fly?</li>
+									<li className="livesInWater" >Does it live in water?</li>
+									<li className="fur" >Does it have fur?</li>
+
+									<li className="famous" a href="#" onClick={randomAnimal.famous === true ? this.addListYes : this.addListNo}>Is it famous?</li>
+									<li className="male" onClick={() => randomAnimal.male}>Is it male?</li>
+									<li className="actor" onClick={randomAnimal.actor === true ? this.addListYes : this.addListNo}>Is it an actor?</li>
+									<li className="athelete" onClick={randomAnimal.athelete === true ? this.addList : null}>Is it an athelete?</li>
+									<li className="musician" a href="#" onClick={randomAnimal.musician === true ? this.addListYes : this.addListNo}>Is it a musician?</li>
 									<li className="politician">Is it a politician?</li>
 									<li className="singer">Is it a singer?</li>
 									<li className="awardWinner">Is it an award winner?</li>
