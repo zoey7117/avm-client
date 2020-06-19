de96f1bdd3f78161d79f90f07d362950c5beef1f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-06-19 18:22:31 -0400

    added click events

--- src/components/animals/Animals.js
+++ src/components/animals/Animals.js
@@ -1,135 +1,102 @@
 import React from 'react';
 import './Animal.css';
-let yes = document.querySelectorAll('.yes');
-let no = document.querySelectorAll('.no');
+
+let questions = {
+	alive: 'Is it alive?',
+	extinct: 'Is is extinct?'
+};
 
 class Animals extends React.Component {
-
-	isClicked() {
-		// if (true) {
-		// 	yes.classList.add('show-yes');
-		// } else {
-		// 	no.classList.add('show-no');
-		// }
+	constructor(props) {
+		super(props);
+		this.state = {
+			yes: [],
+			no: []
+		};
+		// This binding is necessary to make `this` work in the callback
+		this.handleClick = this.handleClick.bind(this);
 	}
 
-addListYes() {
-	document.querySelector(".yes").classList.add('show-yes')
-}
+	handleClick(animal, key, question) {
+		if (animal[key]) {
+			this.setState((state) => {
+				return { yes: [ ...state.yes, question ] };
+			});
+		} else {
+			this.setState((state) => {
+				return { no: [ ...state.no, question ] };
+			});
+		}
+	}
 
-addListNo() {
-	document.querySelector(".no").classList.add('show-no')
-}
-// addList() {
-// 	document.querySelector(".yes").classList.add('show-yes')
+	generateQuestions = (randomAnimal) => {
+		return Object.keys(randomAnimal).map((key) => {
+			return (
+				<li className={key} onClick={() => this.handleClick(randomAnimal, key, questions[key])}>
+					{questions[key]}
+				</li>
+			);
+		});
+	};
 
+	generateQuestionsYes = () => {
+		return this.state.yes.map((question) => {
+			return <li>{question}</li>;
+		});
+	};
 
-
-// 	}
+	generateQuestionsNo = () => {
+		return this.state.no.map((question) => {
+			return <li>{question}</li>;
+		});
+	};
 
 	render() {
 		let animals = this.props.animals.animals;
 		let randomAnimal = animals[Math.floor(Math.random() * animals.length)];
 		console.log('random animal', randomAnimal);
 		let yes = document.querySelectorAll('.flex-animal.yes');
-		let yeses = document.querySelector('.yes');
-
-		let animalEx = document.getElementsByClassName('flex-animal yes')
-		console.log('animalEx', animalEx, 'yes', yes, 'yeses', yeses)
-		console.log(yes)
+		let animalEx = document.getElementsByClassName('flex-animal yes');
+		console.log('animalEx', animalEx, 'yes', yes);
 
 		if (!randomAnimal) return null;
-
 		return (
 			<div>
-				<div>
-					<div className="flex-instructions animal-hed">
-					<p><span>Animal</span>
+				<div className="flex-instructions animal-hed">
+					<p>
+						<span>Animal</span>
 						<br />
-						Is it bigger than a breadbox? </p>
-{/* <strong>{String(randomAnimal.breadbox)}</strong> */}
+						Is it bigger than a breadbox?{' '}
+					</p>
+					{/* <strong>{String(randomAnimal.breadbox)}</strong> */}
+				</div>
 
+				<div className="animal-container">
+					<div className="flex-animal yes">
+						<h1>
+							<ul>
+								<span>yes</span>
+								{this.generateQuestionsYes()}
+							</ul>
+						</h1>
 					</div>
-					<div className="animal-container">
-						<div className="flex-animal yes">
-							<h1>
-								<ul>
-									<span>yes</span>
-									<li className="alive">Is it alive?</li>
-									<li className="extinct">Is it extinct?</li>
-									<li className="mammal">Is it a mammal?</li>
-									<li className="amphibian">Is it an amphibian?</li>
-									<li className="arms">Does it have arms?</li>
-									<li className="legs">Does it have legs?</li>
-									<li className="fly">Does it fly?</li>
-									<li className="livesInWater">Does it live in water?</li>
-									<li className="fur">Does it have fur?</li>
-									<li className="famous">Is it famous?</li>
-									<li className="male">Is it male?</li>
-									<li className="actor">Is it an actor?</li>
-									<li className="athelete">Is it an athelete?</li>
-									<li className="musician">Is it a musician?</li>
-									<li className="politician">Is it a politician?</li>
-									<li className="singer">Is it a singer?</li>
-									<li className="awardWinner">Is it an award winner?</li>
-									<li className="ageAbove40">Is it more than 40 years old?</li>
-								</ul>
-							</h1>
-						</div>
-						<div className="flex-animal question">
-						<span></span>
-							<h1>
-								{/* <hr /> */}
-								<ul>
-									<h3>{String(randomAnimal.breadbox)}</h3>
-									<li className="alive" >Is it alive?</li>
-									<li className="extinct" onClick={() => console.log("extinct clicked!")}>Is it extinct?</li>
-									<li className="mammal" a href="#" onClick={randomAnimal.mammal === true ? this.addListYes : null} >Is it a mammal?</li>
-									<li className="amphibian" onClick={() => console.log("amphibian clicked!", this.props)}>Is it an amphibian?</li>
-									<li className="arms" onClick={() => console.log("arms clicked!", randomAnimal.arms)} >Does it have arms?</li>
-									<li className="legs" onClick={() => this.isClicked(randomAnimal.legs)}>Does it have legs?</li>
-									<li className="fly" >Does it fly?</li>
-									<li className="livesInWater" >Does it live in water?</li>
-									<li className="fur" >Does it have fur?</li>
 
-									<li className="famous" a href="#" onClick={randomAnimal.famous === true ? this.addListYes : this.addListNo}>Is it famous?</li>
-									<li className="male" onClick={() => randomAnimal.male}>Is it male?</li>
-									<li className="actor" onClick={randomAnimal.actor === true ? this.addListYes : this.addListNo}>Is it an actor?</li>
-									<li className="athelete" onClick={randomAnimal.athelete === true ? this.addList : null}>Is it an athelete?</li>
-									<li className="musician" a href="#" onClick={randomAnimal.musician === true ? this.addListYes : this.addListNo}>Is it a musician?</li>
-									<li className="politician">Is it a politician?</li>
-									<li className="singer">Is it a singer?</li>
-									<li className="awardWinner">Is it an award winner?</li>
-									<li className="ageAbove40">Is it more than 40 years old?</li>
-								</ul>
-							</h1>
-							{console.log(randomAnimal)}
-						</div>
-						<div className="flex-animal no">
-							<h1>
-								<ul>
-						<span>no</span>
-								<li className="alive">Is it alive?</li>
-								<li className="extinct">Is it extinct?</li>
-								<li className="mammal">Is it a mammal?</li>
-								<li className="amphibian">Is it an amphibian?</li>
-								<li className="arms">Does it have arms?</li>
-								<li className="legs">Does it have legs?</li>
-								<li className="fly">Does it fly?</li>
-								<li className="livesInWater">Does it live in water?</li>
-								<li className="fur">Does it have fur?</li>
-								<li className="famous">Is it famous?</li>
-								<li className="male">Is it male?</li>
-								<li className="actor">Is it an actor?</li>
-								<li className="athelete">Is it an athelete?</li>
-								<li className="musician">Is it a musician?</li>
-								<li className="politician">Is it a politician?</li>
-								<li className="singer">Is it a singer?</li>
-								<li className="awardWinner">Is it an award winner?</li>
-									<li className="ageAbove40">Is it more than 40 years old?</li>
-									</ul>
-							</h1>
-						</div>
+					<div className="flex-animal question">
+						<h1>
+							<ul>
+								<h3>{String(randomAnimal.breadbox)}</h3>
+								{this.generateQuestions(randomAnimal)}
+							</ul>
+						</h1>
+					</div>
+
+					<div className="flex-animal no">
+						<h1>
+							<ul>
+								<span>no</span>
+								{this.generateQuestionsNo()}
+							</ul>
+						</h1>
 					</div>
 				</div>
 			</div>
@@ -137,36 +104,3 @@ addListNo() {
 	}
 }
 export default Animals;
-// class LoginControl extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleLoginClick = this.handleLoginClick.bind(this);
-//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
-//     this.state = {isLoggedIn: false};
-//   }
-
-//   handleLoginClick() {
-//     this.setState({isLoggedIn: true});
-//   }
-
-//   handleLogoutClick() {
-//     this.setState({isLoggedIn: false});
-//   }
-
-//   render() {
-//     const isLoggedIn = this.state.isLoggedIn;
-//     let button;
-//     if (isLoggedIn) {
-//       button = <LogoutButton onClick={this.handleLogoutClick} />;
-//     } else {
-//       button = <LoginButton onClick={this.handleLoginClick} />;
-//     }
-
-//     return (
-//       <div>
-//         <Greeting isLoggedIn={isLoggedIn} />
-//         {button}
-//       </div>
-//     );
-//   }
-// }
